28b0526d8cac2736180a54b0df1f59affff305d8
jhus <j.husi@staubli.com>
2024-07-24 14:25:03 +0200

    Code cleanup

--- FFXIVcraftPricing.py
+++ FFXIVcraftPricing.py
@@ -1,10 +1,9 @@
 import json
 import os
 import requests
-import sys
 
 
-WORLD = 'Shiva' # id: 67
+WORLD = 'Shiva'  # id: 67
 
 XIVAPI_URL = 'https://xivapi.com/'
 UNIVERSALIS_URL = 'https://universalis.app/api/v2/'
@@ -15,14 +14,15 @@ UNIVERSALIS_RATE_LIMIT = 25
 
 CHECK_CERT = False   # deactivate if SSL errors occur
 
-def get_cache_folder(cache_type):
+
+def get_cache_folder(cache_type: str) -> str:
     if cache_type == 'recipe':
         return 'cache/recipes/'
     if cache_type == 'icon':
         return 'cache/icons/'
 
 
-def cache(cache_type=None, data=None):
+def cache(cache_type: str = None, data: dict = None) -> None:
     try:
         os.mkdir('cache')
         os.mkdir('cache/recipes')
@@ -42,23 +42,23 @@ def cache(cache_type=None, data=None):
         f.write(json.dumps(data))
         
 
-def is_cached(obj_id, cache_type):
+def is_cached(obj_id: int, cache_type=None) -> bool:
     folder = 'cache/recipes/'
     return os.path.isfile(f"{folder}{obj_id}.json")
     
 
-def get_cache(obj_id, cache_type):
+def get_cache(obj_id: int, cache_type: str) -> dict:
     folder = get_cache_folder(cache_type)
     with open(f'{folder}{obj_id}.json', 'r', encoding='UTF-8') as f:
         return json.loads(f.read())
     
 
-def get_json_from_api(url):
+def get_json_from_api(url: str) -> dict:
     res = requests.get(url, verify=CHECK_CERT)  # ssl? stupid corporate proxy/firewall
     return json.loads(res.text)
     
 
-def item_name_to_id(name):
+def item_name_to_id(name: str) -> int:
     url = f"{XIVAPI_URL}search?string={name}"
     search = get_json_from_api(url)
         
@@ -70,23 +70,23 @@ def item_name_to_id(name):
     return id_
     
 
-def get_recipe_json(recipe_id):
+def get_recipe_json(recipe_id: int) -> dict:
     url = f"{XIVAPI_URL}Recipe/{recipe_id}"
     return get_json_from_api(url)
     
     
-def get_recipe_tree(recipe_id):
+def get_recipe_tree(recipe_id: int) -> dict:
     if is_cached(recipe_id, cache_type='recipe'):
-        return get_cache(recipe_id)
+        return get_cache(recipe_id, 'recipe')
     else:
         json_obj = get_recipe_json(recipe_id)
-        item = {}
-        item['name'] = json_obj['ItemResult']['Name']
-        item['id'] = json_obj['ItemResult']['ID']
-        item["icon"] = json_obj['Icon'],
-        item['ingredients'] = []
+        item = {
+            'name': json_obj['ItemResult']['Name'],
+            'id': json_obj['ItemResult']['ID'],
+            "icon": (json_obj['Icon'],),
+            'ingredients': []
+        }
         node = json_obj
-        # print(item['name'])
         for i in range(8):
             if node['AmountIngredient' + str(i)] != 0:
                 item['ingredients'].append({
@@ -100,14 +100,12 @@ def get_recipe_tree(recipe_id):
         
         for idx, ingredient in enumerate(item['ingredients']):
             index = idx
-            # print(" -", ingredient['name'], "x" + str(ingredient['amount']))
             subnodes = node['ItemIngredientRecipe' + str(index)]
             if subnodes is None:
                 continue
             subnode = subnodes[0]  # if there are multiple jobs who can create the item, this has a multiple elements
             for i in range(8):
                 if subnode['AmountIngredient' + str(i)] != 0:
-                    # print("   -", subnode['ItemIngredient' + str(i)]['Name'], "x" + str(subnode['AmountIngredient' + str(i)]))
                     item['ingredients'][index]['ingredients'].append({
                         "index": i,
                         "id": subnode['ItemIngredient' + str(i)]['ID'],
@@ -116,15 +114,14 @@ def get_recipe_tree(recipe_id):
                         "icon": subnode['ItemIngredient' + str(i)]['Icon']
                     })
                     item['ingredients'][index]["amount_result"] = subnode['AmountResult']
-        # print('\n')
         cache(cache_type='recipe', data=item)
 
     return item
     
     
-def get_prices(item):
+def get_prices(item: dict) -> dict:
     # https://universalis.app/api/v2/Shiva/43996?listings=5&entries=0
-    # |--------- base url ---------|-world|-ids*-|         ^         ^
+    # |--------- base url ---------|-world|-ids*-|        ^         ^
     #                           how many listings per item?      history size
     # * can be a single id or a comma-separated list of ids
     item_ids = [item['id']]
@@ -134,7 +131,6 @@ def get_prices(item):
             item_ids.append(itm_2['id'])
 
     item_ids = list(set(item_ids))
-    # print(item_ids)
 
     url = f"{UNIVERSALIS_URL}{WORLD}/{','.join(str(itm) for itm in item_ids)}"
     url += "?listings=10&entries=0"
@@ -185,29 +181,23 @@ def display_result(item):  # name subject to change
     txt = ''
     txt += f"{item['name']:24.23}{'#':>4}{'craft':>7}{'buy':>7}\n"
     for ingredient in item['ingredients']:
-        txt += f"  {ingredient['name']:22.21}{ingredient['amount']:>4}{ingredient.get('price_if_crafted', ''):>7}{ingredient['price']:>7}\n"
+        txt += (f"  {ingredient['name']:22.21}{ingredient['amount']:>4}{ingredient.get('price_if_crafted', ''):>7}"
+                f"{ingredient['price']:>7}\n")
         for sub_ingredient in ingredient['ingredients']:
             txt += f"    {sub_ingredient['name']:20.19}{sub_ingredient['amount']:4}{'':>7}{sub_ingredient['price']:>7}\n"
     return txt
     
 
 def generate_result(item_name):
-    #item_name = sys.argv[1]
+    # item_name = sys.argv[1]
     cache()
     recipe_id = item_name_to_id(item_name)
     item = get_recipe_tree(recipe_id)
     item = get_prices(item)
     return item
-    #return display_result(item)
+    # return display_result(item)
 
-    
-# main()
-#main("Acqua Pazza")
 
-# 42459 = Book of Ra'Kaznar (item) vs 
-# 5985  = Recipe
 # TODO
 # - fetch icons online
 # - cache icons
-# - UI
-# - fix calculation for multicraft (e.g. Sanctified Water which makes 3 and therefore makes it cheaper to craft (*1/3)
--- FFXIVcraftPricing_GUI.py
+++ FFXIVcraftPricing_GUI.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtCore import Qt
-import FFXIVcraftPricing as XIVCP
+import FFXIVcraftPricing as XIVcp
 
 
 BASE_TITLE = "XIV Craft Pricing"
@@ -13,13 +13,13 @@ class TableModel(QtCore.QAbstractTableModel):
         self._data = data
         self.columns = ["Item", "Amount", "P(c)", "P(b)"]
     
-    def headerData(self, section, orientation, role):
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = ...):
         if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
             return self.columns[section]
         if orientation == Qt.Orientation.Vertical and role == Qt.ItemDataRole.DisplayRole:
             return ""
     
-    def data(self, index, role):
+    def data(self, index: QtCore.QModelIndex, role: int = ...):
         if role == Qt.ItemDataRole.DisplayRole:
             return self._data[index.row()][index.column()]
             
@@ -34,16 +34,11 @@ class TableModel(QtCore.QAbstractTableModel):
             if index.column() == 2:
                 if value <= int(self._data[index.row()][index.column()+1] or 0):
                     return QtGui.QColor('#f1ffe8')
-                #if self._data[index.row()][index.column()+1] == '':
-                    
 
-    def rowCount(self, index):
-        # The length of the outer list.
+    def rowCount(self, index: int = ...):
         return len(self._data)
 
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
+    def columnCount(self, index: int = ...):
         return len(self._data[0])
 
 
@@ -58,10 +53,10 @@ class MainWindow(QtWidgets.QMainWindow):
 
         search_str = "Acqua Pazza"
         self.setWindowTitle(BASE_TITLE + ' - ' + search_str)
-        #data = XIVCP.generate_result(search_str)
+        # data = XIVcp.generate_result(search_str)
         with open('tmp.json', 'r', encoding="utf-8") as f:
             import json
-            #f.write(json.dumps(data))
+            # f.write(json.dumps(data))
             data = json.loads(f.read())
 
         data_tbl = []
@@ -86,7 +81,6 @@ class MainWindow(QtWidgets.QMainWindow):
                 ])
         
         self.model = TableModel(data_tbl)
-        
 
         self.table.setModel(self.model)
         self.setCentralWidget(self.table)
@@ -96,13 +90,14 @@ class MainWindow(QtWidgets.QMainWindow):
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeMode.Fixed)
         header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeMode.Fixed)
         header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeMode.Fixed)
-        
+
+        # window with = 500. half the width is shared between 3 cols.
         header.resizeSection(1, int(500/2/3))
         header.resizeSection(2, int(500/2/3))
         header.resizeSection(3, int(500/2/3))
 
 
-app=QtWidgets.QApplication(sys.argv)
-window=MainWindow()
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
 window.show()
 sys.exit(app.exec())
